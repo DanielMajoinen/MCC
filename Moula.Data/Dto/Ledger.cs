33863b8e361171d9434a3e847f393f54a6f6f0d3
daniel <daniel@majoinen.com>
2020-11-23 22:45:12 +1100

    add Ledger and ILedgerRepository

---
+++ Moula.Data/Dto/Ledger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Moula.Data.Dto
+{
+    public class Ledger
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public string ClosedReason { get; set; }
+    }
+}
---
+++ Moula.Data/Repositories/ILedgerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Moula.Data.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Moula.Data.Repositories
+{
+    interface ILedgerRepository
+    {
+        Task<IEnumerable<Ledger>> GetAccountLedger(int accountId);
+    }
+}
