ef72aa2a001b5ce74f62e46594e6b63e4f7de35b
daniel <daniel@majoinen.com>
2020-11-24 00:15:04 +1100

    add Ledger contract model object

---
+++ Moula.Contracts/Ledger.cs
@@ -0,0 +1,11 @@
+﻿namespace Moula.Contracts
+{
+    public class Ledger
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public string ClosedReason { get; set; }
+    }
+}
