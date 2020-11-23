12e000eea1f2d088cdb5327e9fa1ce582ff02093
daniel <daniel@majoinen.com>
2020-11-24 00:15:12 +1100

    add LedgerController

---
+++ Moula.Api/Controllers/LedgerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moula.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Moula.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LedgerController : ControllerBase
+    {
+        private readonly ILogger<LedgerController> _logger;
+
+        public LedgerController(ILogger<LedgerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Ledger> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
